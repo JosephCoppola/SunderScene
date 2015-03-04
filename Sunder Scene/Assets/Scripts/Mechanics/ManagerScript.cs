fb509aaef56446b48d548b73e71138d63c0897c6
JosephCoppola <jjc9718@rit.edu>
2015-03-04 00:50:38 -0500

    Final Project Files
    Game made for GDD II at RIT

---
+++ Sunder Scene/Assets/Scripts/Mechanics/ManagerScript.cs
@@ -0,0 +1,196 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ManagerScript : MonoBehaviour {
+
+	// References Set in Inspector
+	public Transform player;
+
+	// Prefabs
+	public GameObject joePrefab;
+	public GameObject topPrefab;
+	public GameObject hammerPrefab;
+
+	// Enemy Managerment variables
+	private List<Transform> enemies;
+	private int spawnCap;
+	private bool canSpawn;
+
+	private bool startGame;
+	private bool showHowTo;
+	private string instructions;
+	private bool gameOver;
+
+	void Start()
+	{
+		startGame = false;
+		showHowTo = false;
+		canSpawn = true;
+		gameOver = false;
+		spawnCap = 20;
+		enemies = new List<Transform>();
+
+		CreateInstructions();
+	}
+
+	void Update()
+	{
+		if(startGame)
+		{
+			if(enemies.Count < spawnCap && canSpawn)
+			{
+				int num = Random.Range(0, spawnPoints.Length);
+				StartCoroutine(SpawnDelay(spawnPoints[num].position));
+			}
+		}
+	}
+
+	void OnGUI()
+	{
+		if(!startGame)
+		{
+			GUI.Box(new Rect(Screen.width * 0.4f, Screen.height * 0.25f, Screen.width * 0.2f, Screen.height * 0.5f), "Sunder Scene");
+			if(!showHowTo)
+			{
+				if(GUI.Button(new Rect(Screen.width * 0.45f, Screen.height * 0.5f - Screen.width * 0.1f, Screen.width * 0.1f, Screen.width * 0.05f), "Play"))
+				{
+					startGame = true;
+				}
+				if(GUI.Button(new Rect(Screen.width * 0.45f, Screen.height * 0.5f - Screen.width * 0.025f, Screen.width * 0.1f, Screen.width * 0.05f), "How To Play"))
+				{
+					showHowTo = true;
+				}
+				if(Application.platform != RuntimePlatform.OSXWebPlayer && Application.platform != RuntimePlatform.WindowsWebPlayer)
+				{
+					if(GUI.Button(new Rect(Screen.width * 0.45f, Screen.height * 0.5f + Screen.width * 0.05f, Screen.width * 0.1f, Screen.width * 0.05f), "Quit"))
+					{
+						Application.Quit();
+					}
+				}
+			}
+			else
+			{
+				GUI.Label(new Rect(Screen.width * 0.42f, Screen.height * 0.3f, Screen.width * 0.8f, Screen.height * 0.6f), instructions);
+				if(GUI.Button(new Rect(Screen.width * 0.45f, Screen.height * 0.5f + Screen.width * 0.05f, Screen.width * 0.1f, Screen.width * 0.05f), "Back"))
+				{
+					showHowTo = false;
+				}
+			}
+		}
+		else if(gameOver)
+		{
+			//GUI.Box(new Rect(Screen.width * 0.4f, Screen.height * 0.25f, Screen.width * 0.2f, Screen.height * 0.5f), "Game Over");
+			if(GUI.Button(new Rect(Screen.width * 0.45f, Screen.height * 0.5f - Screen.width * 0.025f, Screen.width * 0.1f, Screen.width * 0.05f), "Retry"))
+			{
+				Application.LoadLevel(0);
+			}
+		}
+	}
+
+	public bool GameStarted
+	{
+		get { return startGame; }
+	}
+
+	public List<Transform> Enemies
+	{
+		get { return enemies; }
+	}
+
+	private void SpawnAJoe(Vector3 pos)
+	{
+		GameObject betterJoe = Instantiate(joePrefab,
+		                                   pos,
+		                                   Quaternion.identity) as GameObject;
+		betterJoe.GetComponent<JoeScript>().Initialize(this, player);
+		enemies.Add(betterJoe.transform);
+	}
+
+	private void SpawnATop(Vector3 pos)
+	{
+		GameObject betterJoe = Instantiate(topPrefab,
+		                                   pos,
+		                                   Quaternion.identity) as GameObject;
+		betterJoe.GetComponent<TopScript>().Initialize(this, player);
+		enemies.Add(betterJoe.transform);
+	}
+	private void SpawnAHammer(Vector3 pos)
+	{
+		GameObject betterJoe = Instantiate(hammerPrefab,
+		                                   pos,
+		                                   Quaternion.identity) as GameObject;
+		betterJoe.GetComponent<HammerScript>().Initialize(this, player);
+		enemies.Add(betterJoe.transform);
+	}
+
+	public void RemoveEnemy(Transform enemy)
+	{
+		enemies.Remove(enemy);
+	}
+
+	public Transform[] spawnPoints;
+
+	private IEnumerator SpawnDelay(Vector3 pos)
+	{
+		canSpawn = false;
+		yield return new WaitForSeconds(.25f);
+		SpawnAJoe(pos);
+		canSpawn = true;
+	}
+
+	public void UpdateSpawnCap(int kills)
+	{
+		if(kills % 20 == 0)
+		{
+			int num = Random.Range(0, spawnPoints.Length);
+			SpawnAHammer(spawnPoints[num].position);
+			spawnCap++;
+		}
+		else if(kills % 5 == 0)
+		{
+			int num = Random.Range(0, spawnPoints.Length);
+			SpawnATop(spawnPoints[num].position);
+
+			int chance = Random.Range(0, 100);
+
+			if(chance < 40)
+			{
+				SpawnATop(spawnPoints[(num+2)%4].position);
+			}
+			if(chance < 20)
+			{
+				SpawnATop(spawnPoints[(num+1)%4].position);
+			}
+			if(chance < 10)
+			{
+				SpawnATop(spawnPoints[(num+3)%4].position);
+			}
+		}
+	}
+
+	public void GameOver()
+	{
+		gameOver = true;
+		Screen.lockCursor = false;
+	}
+
+	private void CreateInstructions()
+	{
+		instructions = "WASD/Arrow Keys to move";
+		instructions += "\n";
+		instructions += "\nClick and move the mouse to swing your sword";
+		instructions += "\n";
+		instructions += "\nP to pause";
+		instructions += "\n";
+		instructions += "\nEsc to unlock the cursor";
+		instructions += "\n";
+		instructions += "\nHit enemies with your sword to kill them";
+		instructions += "\n";
+		instructions += "\nHitting enemies also increases your combo";
+		instructions += "\n";
+		instructions += "\nHigh combos get you mad points";
+		instructions += "\n";
+		instructions += "\nDon't Die";
+	}
+}
---
+++ Sunder Scene/Assets/Scripts/Mechanics/PlayerScript.cs
@@ -0,0 +1,258 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+	public ManagerScript mngr;
+	public WeaponScript weaponScript;
+	public Transform swordPivot;
+	public Transform sword;
+	public Transform body;
+	public Transform feet;
+	public Camera calcCam;
+	public GUIText healthDisplay;
+	public GUIText scoreDisplay;
+
+	private Vector3 mousePos;
+	private Vector3 swordVel;
+	private Transform target;
+	private int targetNum;
+
+	private float swordVelMult = 50.0f;
+	private float swordVelDamp = 0.25f;
+
+	private Animator swordAnimator;
+	private Animator bodyAnimator;
+	private Animator feetAnimator;
+
+	private bool alive;
+	private int health;
+	private bool flinch;
+	private float distToGround;
+	private int score;
+	private int kills;
+
+	private AudioSource audioSource;
+
+	void Start()
+	{
+		kills = 0;
+		score = 0;
+		alive = true;
+		flinch = false;
+		health = 500;
+		targetNum = 0;
+		mousePos = new Vector3(Screen.width, Screen.height*0.5f, 100);
+		swordVel = Vector3.zero;
+
+		swordAnimator = sword.GetComponent<Animator>();
+		bodyAnimator = body.GetComponent<Animator>();
+		feetAnimator = feet.GetComponent<Animator>();
+
+		distToGround = collider.bounds.extents.y;
+
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void Update()
+	{
+		if(alive && !flinch && mngr.GameStarted)
+		{
+			HandleInput();
+
+			if(target != null)
+			{
+				transform.LookAt(new Vector3(target.position.x, transform.position.y, target.position.z));
+			}
+		}
+
+		swordVel *= swordVelDamp;
+	}
+
+	public bool Alive
+	{
+		get { return alive; }
+	}
+
+	private bool IsGrounded()
+	{
+		return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+	}
+
+	private void HandleInput()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			Screen.lockCursor = true;
+		}
+
+		DoMovement();
+
+		if(Input.GetKeyDown(KeyCode.Tab))
+		{
+			if(mngr.Enemies.Count > 0)
+			{
+				targetNum = (targetNum+1)%mngr.Enemies.Count;
+				target = mngr.Enemies[targetNum];
+			}
+		}
+
+		if(Input.GetMouseButton(0) || Input.GetMouseButton(1))
+		{
+			DoSlash();
+		}
+		else
+		{
+			Vector3 mouseOffset = new Vector3(0, Input.GetAxis("Mouse X"), 0f);
+			transform.Rotate(mouseOffset * 2, Space.Self);
+		}
+	}
+
+	private void DoMovement()
+	{
+		float vert = Input.GetAxisRaw("Vertical");
+		float hor = Input.GetAxisRaw("Horizontal");
+		if(IsGrounded())
+		{
+			if(vert != 0 || hor != 0)
+			{
+				Vector3 velocity = new Vector3(0, rigidbody.velocity.y, 0);
+				velocity += transform.forward * vert;
+				velocity += transform.right * hor;
+				rigidbody.velocity = velocity.normalized * 10;
+				feetAnimator.SetBool("Moving", true);
+
+				if(vert >= 0)
+				{
+					feet.localRotation = Quaternion.AngleAxis(Mathf.Rad2Deg * Mathf.Atan2(hor, vert), feet.up);
+				}
+				else
+				{
+					feet.localRotation = Quaternion.AngleAxis(Mathf.Rad2Deg * Mathf.Atan2(-hor, -vert), feet.up);
+				}
+			}
+			else
+			{
+				rigidbody.velocity = new Vector3(0, rigidbody.velocity.y, 0);
+				feetAnimator.SetBool("Moving", false);
+			}
+		}
+		else
+		{
+			feetAnimator.SetBool("Moving", false);
+		}
+
+		feetAnimator.SetFloat("VertSpeed", vert);
+		feetAnimator.SetFloat("HorSpeed", hor);
+	}
+
+	private void DoSlash()
+	{
+		Vector3 mouseOffset = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0f);
+		swordVel += mouseOffset * swordVelMult;
+		mousePos += swordVel;
+		mousePos.x = Mathf.Clamp(mousePos.x, -Screen.width * 2, Screen.width * 2);
+		mousePos.y = Mathf.Clamp(mousePos.y, -Screen.height, Screen.height * 2);
+
+		if(mouseOffset.sqrMagnitude > 1f)
+		{
+			float angle = Mathf.Rad2Deg * Mathf.Atan2(mouseOffset.y, mouseOffset.x) + 90;
+			sword.localEulerAngles = new Vector3(0, 0, angle);
+		}
+
+		int amt = Mathf.FloorToInt(swordVel.magnitude * 0.1f);
+		weaponScript.DetectCollision(Mathf.FloorToInt(amt));
+
+		swordPivot.LookAt(calcCam.ScreenToWorldPoint(mousePos));
+	}
+
+	public void Die()
+	{
+		alive = false;
+		
+		feetAnimator.SetBool("Moving", false);
+		bodyAnimator.SetBool("Moving", false);
+
+		swordPivot.localEulerAngles = new Vector3(0, 90, 0);
+		swordAnimator.enabled = true;
+		swordAnimator.SetTrigger("Death");
+		feetAnimator.SetTrigger("Death");
+		bodyAnimator.SetBool("Death", true);
+		rigidbody.velocity = -Vector3.up;
+
+		mngr.GameOver();
+
+		StartCoroutine(DisableDelay());
+	}
+
+	public void Flinch()
+	{
+		if(!flinch)
+		{
+			audioSource.Play();
+			flinch = true;
+			feetAnimator.SetTrigger("Flinch");
+			bodyAnimator.SetTrigger("Flinch");
+			bodyAnimator.SetBool("Moving", false);
+			feetAnimator.SetBool("Moving", false);
+
+			StartCoroutine(FlinchDelay());
+		}
+	}
+
+	private IEnumerator FlinchDelay()
+	{
+		yield return new WaitForSeconds(0.25f);
+		flinch = false;
+		bodyAnimator.SetBool("Flinch", false);
+	}
+
+	private IEnumerator DisableDelay()
+	{
+		yield return new WaitForSeconds(1.0f);
+		if(IsGrounded())
+		{
+			rigidbody.isKinematic = true;
+			collider.enabled = false;
+		}
+		else
+		{
+			StartCoroutine(DisableDelay());
+		}
+	}
+
+	public void DoDamage(int amt)
+	{
+		health -= amt;
+		UpdateHealth();
+		weaponScript.ResetCombo();
+
+		if(health <= 0)
+		{
+			health = 0;
+			Die ();
+		}
+		else
+		{
+			Flinch();
+		}
+	}
+
+	private void UpdateHealth()
+	{
+		healthDisplay.text = "Health: " + Mathf.RoundToInt((float)health / 500 * 100) + "%";
+	}
+
+	public void AddToScore(int amt)
+	{
+		score += amt;
+		kills++;
+		UpdateScore();
+	}
+
+	private void UpdateScore()
+	{
+		scoreDisplay.text = "Score: " + score;
+		mngr.UpdateSpawnCap(kills);
+	}
+}
